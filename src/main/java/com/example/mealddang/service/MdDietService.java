e44182292825bfeec0e4a1b4a4dd7b1bde0c1a8b
byepingu <3149nelle@gmail.com>
2024-03-13 21:21:30 +0900

    밀땅일지 오류 수정(유저정보 없을 경우 기본값 설정)

--- src/main/java/com/example/mealddang/service/MdDietService.java
+++ src/main/java/com/example/mealddang/service/MdDietService.java
@@ -14,8 +14,30 @@ public class MdDietService {
     private MdDietRepository mdDietRepository;
     
     public MdDiet getDiet(MdUser mduser) {
-        String gender = mduser.getGender();
-        Integer birthyear = Integer.parseInt(mduser.getBirth());
+
+        String gender;
+        // mduser.getGender()가 null 또는 빈 문자열인지 확인
+        if (mduser.getGender() != null && !mduser.getGender().isEmpty()) {
+            gender = mduser.getGender();
+        } else {
+            // mduser.getGender()가 null일 때의 처리
+            gender = "M";
+        }
+
+        Integer birthyear;
+        // mduser.getBirth()가 null 또는 빈 문자열인지 확인
+        if (mduser.getBirth() != null && !mduser.getBirth().isEmpty()) {
+            try {
+                birthyear = Integer.parseInt(mduser.getBirth());
+            } catch (NumberFormatException e) {
+                // 숫자로 변환할 수 없을 때의 예외 처리
+                birthyear = 1999;
+            }
+        } else {
+            // mduser.getBirth()가 null일 때의 처리
+            birthyear = 1999;
+        }
+        
         return mdDietRepository.getDiet(gender, birthyear);
     }
 }
