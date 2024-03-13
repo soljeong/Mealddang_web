f417a39a56647daab51fd33f2f9dbf1ff62b6dc3
hyul77 <kimhyul77@gmail.com>
2024-03-13 20:59:07 +0900

    지도 필터 끝

--- src/main/java/com/example/mealddang/model/repository/MdKeyRepository.java
+++ src/main/java/com/example/mealddang/model/repository/MdKeyRepository.java
@@ -10,12 +10,8 @@ import org.springframework.data.repository.query.Param;
 import com.example.mealddang.model.entity.MdKey;
 
 public interface MdKeyRepository extends JpaRepository<MdKey,Long>{
-
-    // @Query(value = "SELECT COUNT(DISTINCT restaurant_id) FROM md_restaurant_keyword WHERE keyword = :searchkey", nativeQuery = true)
-    // int countByKeyword(@Param("searchkey") String searchkey);
     @Query(value = "SELECT DISTINCT rk.keyword_id, msm.store_id_n, msm.store_name, msm.store_phone, msm.store_road_address_n, msm.store_x, msm.store_y, msm.store_image_url, msm.store_menu_url_n " +
     "FROM md_restaurant_keyword rk JOIN md_store_master msm ON rk.restaurant_id = msm.store_id_n " +
     "WHERE keyword = :keyword LIMIT 30", nativeQuery = true)
     public List<MdKey> findByKeyword(@Param(value="keyword") String keyword);
-    
 }
