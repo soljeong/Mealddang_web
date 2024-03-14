7dc3bd3d1a1ba123f54798dc09d6ea48c9da6b12
dayeh512 <ququp@naver.com>
2024-03-14 17:19:01 +0900

    잡다한것해결

--- src/main/resources/static/js/weekly.js
+++ src/main/resources/static/js/weekly.js
@@ -72,6 +72,8 @@ option = {
         top: '5%',
         left: 'center'
     },
+
+    //권장그래프
     series: [
         {
             name: 'Access From',
@@ -98,13 +100,14 @@ option = {
                 show: false
             },
             data: [
-                { value: 1048, name: 'Search Engine' },
-                { value: 735, name: 'Direct' },
-                { value: 580, name: 'Email' },
-                { value: 484, name: 'Union Ads' },
-                { value: 300, name: 'Video Ads' }
+                { value: 38.8, name: '탄수화물' },
+                { value: 24.3, name: '단백질' },
+                { value: 34.0, name: '지방' },
+                { value: 2.3, name: '식이섬유' },
+                { value: 0.6, name: '당' }
             ]
         },
+        //실제 섭취 그래프
         {
             name: 'Access From',
             type: 'pie',
@@ -130,99 +133,14 @@ option = {
                 show: false
             },
             data: [
-                { value: 1048, name: 'Search Engine' },
-                { value: 735, name: 'Direct' },
-                { value: 580, name: 'Email' },
-                { value: 484, name: 'Union Ads' },
-                { value: 300, name: 'Video Ads' }
+                { value: 38.8, name: '탄수화물' },
+                { value: 24.3, name: '단백질' },
+                { value: 34.0, name: '지방' },
+                { value: 2.3, name: '식이섬유' },
+                { value: 0.6, name: '당' }
             ]
         }
     ]
 };
 
 option && myChart.setOption(option);
-
-//원그래프
-// var chartDom = document.getElementById('main2');
-// var myChart = echarts.init(chartDom);
-// var option;
-
-//         option = {
-//             title: {
-//                 text: '3대 영양소 섭취 비율',
-//                 subtext: 'kcal 기준',
-//                 left: 'center'
-//             },
-//             tooltip: {
-//                 trigger: 'item',
-//                 formatter: '{a} <br/>{b} : {c} ({d}%)'
-//             },
-//             legend: {
-//                 left: 'center',
-//                 top: 'bottom',
-//                 data: [
-//                 'rose1',
-//                 'rose2',
-//                 'rose3',
-//                 'rose4',
-//                 'rose5'
-//                 ]
-//             },
-//             toolbox: {
-//                 show: true,
-//                 feature: {
-//                 mark: { show: true },
-//                 dataView: { show: true, readOnly: false },
-//                 restore: { show: true },
-//                 saveAsImage: { show: true }
-//                 }
-//             },
-//             series: [
-//                 {
-//                 name: 'Radius Mode',
-//                 type: 'pie',
-//                 radius: [20, 140],
-//                 center: ['25%', '50%'],
-//                 roseType: 'radius',
-//                 itemStyle: {
-//                     borderRadius: 5
-//                 },
-//                 label: {
-//                     show: false
-//                 },
-//                 emphasis: {
-//                     label: {
-//                     show: true
-//                     }
-//                 },
-//                 data: [
-//                     { value: 38.8, name: '탄수화물' },
-//                     { value: 24.3, name: '단백질' },
-//                     { value: 34.0, name: '지방' },
-//                     { value: 2.3, name: '식이섬유' },
-//                     { value: 0.6, name: '당' }
-//                 ]
-//                 },
-//                 {
-//                 name: 'Area Mode',
-//                 type: 'pie',
-//                 radius: [20, 140],
-//                 center: ['75%', '50%'],
-//                 roseType: 'area',
-//                 itemStyle: {
-//                     borderRadius: 5
-//                 },
-//                 data: [
-//                     { value: 30, name: 'rose 1' },
-//                     { value: 28, name: 'rose 2' },
-//                     { value: 26, name: 'rose 3' },
-//                     { value: 24, name: 'rose 4' },
-//                     { value: 22, name: 'rose 5' },
-//                     { value: 20, name: 'rose 6' },
-//                     { value: 18, name: 'rose 7' },
-//                     { value: 16, name: 'rose 8' }
-//                 ]
-//                 }
-//             ]
-//             };
-//             option && myChart.setOption(option);
