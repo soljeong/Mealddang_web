934a23a79b23cb188c6113d1debbae7e3f926b00
byepingu <3149nelle@gmail.com>
2024-03-14 15:59:35 +0900

    도넛그래프

--- src/main/resources/static/js/weekly.js
+++ src/main/resources/static/js/weekly.js
@@ -24,9 +24,9 @@ import * as echarts from 'https://cdn.jsdelivr.net/npm/echarts/dist/echarts.esm.
                 bottom: 50
                 };
             const series = [
-                'tan',
-                'dan',
-                'zi',
+                '탄수화물',
+                '단백질',
+                '지방',
                 ].map((name, sid) => {
                 return {
                 name,
@@ -59,97 +59,170 @@ import * as echarts from 'https://cdn.jsdelivr.net/npm/echarts/dist/echarts.esm.
 
                 option && myChart.setOption(option);
 
-
-//원그래프
+// 도넛 그래프
 var chartDom = document.getElementById('main2');
 var myChart = echarts.init(chartDom);
 var option;
 
-        option = {
-            title: {
-                text: 'Nightingale Chart',
-                subtext: 'Fake Data',
-                left: 'center'
+option = {
+    tooltip: {
+        trigger: 'item'
+    },
+    legend: {
+        top: '5%',
+        left: 'center'
+    },
+    series: [
+        {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['15%', '40%'],
+            center: ['25%', '50%'],
+            avoidLabelOverlap: false,
+            padAngle: 5,
+            itemStyle: {
+                borderRadius: 10
             },
-            tooltip: {
-                trigger: 'item',
-                formatter: '{a} <br/>{b} : {c} ({d}%)'
+            label: {
+                show: false,
+                position: 'center'
             },
-            legend: {
-                left: 'center',
-                top: 'bottom',
-                data: [
-                'rose1',
-                'rose2',
-                'rose3',
-                'rose4',
-                'rose5',
-                'rose6',
-                'rose7',
-                'rose8'
-                ]
-            },
-            toolbox: {
-                show: true,
-                feature: {
-                mark: { show: true },
-                dataView: { show: true, readOnly: false },
-                restore: { show: true },
-                saveAsImage: { show: true }
-                }
-            },
-            series: [
-                {
-                name: 'Radius Mode',
-                type: 'pie',
-                radius: [20, 140],
-                center: ['25%', '50%'],
-                roseType: 'radius',
-                itemStyle: {
-                    borderRadius: 5
-                },
+            emphasis: {
                 label: {
-                    show: false
-                },
-                emphasis: {
-                    label: {
-                    show: true
-                    }
-                },
-                data: [
-                    { value: 40, name: 'rose 1' },
-                    { value: 33, name: 'rose 2' },
-                    { value: 28, name: 'rose 3' },
-                    { value: 22, name: 'rose 4' },
-                    { value: 20, name: 'rose 5' },
-                    { value: 15, name: 'rose 6' },
-                    { value: 12, name: 'rose 7' },
-                    { value: 10, name: 'rose 8' }
-                ]
-                },
-                {
-                name: 'Area Mode',
-                type: 'pie',
-                radius: [20, 140],
-                center: ['75%', '50%'],
-                roseType: 'area',
-                itemStyle: {
-                    borderRadius: 5
-                },
-                data: [
-                    { value: 30, name: 'rose 1' },
-                    { value: 28, name: 'rose 2' },
-                    { value: 26, name: 'rose 3' },
-                    { value: 24, name: 'rose 4' },
-                    { value: 22, name: 'rose 5' },
-                    { value: 20, name: 'rose 6' },
-                    { value: 18, name: 'rose 7' },
-                    { value: 16, name: 'rose 8' }
-                ]
+                show: true,
+                fontSize: 15,
+                fontWeight: 'bold'
                 }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                { value: 1048, name: 'Search Engine' },
+                { value: 735, name: 'Direct' },
+                { value: 580, name: 'Email' },
+                { value: 484, name: 'Union Ads' },
+                { value: 300, name: 'Video Ads' }
             ]
-            };
-            
-            option && myChart.setOption(option);
+        },
+        {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['15%', '40%'],
+            center: ['75%', '50%'],
+            avoidLabelOverlap: false,
+            padAngle: 5,
+            itemStyle: {
+                borderRadius: 10
+            },
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                show: true,
+                fontSize: 15,
+                fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                { value: 1048, name: 'Search Engine' },
+                { value: 735, name: 'Direct' },
+                { value: 580, name: 'Email' },
+                { value: 484, name: 'Union Ads' },
+                { value: 300, name: 'Video Ads' }
+            ]
+        }
+    ]
+};
 
+option && myChart.setOption(option);
 
+//원그래프
+// var chartDom = document.getElementById('main2');
+// var myChart = echarts.init(chartDom);
+// var option;
+
+//         option = {
+//             title: {
+//                 text: '3대 영양소 섭취 비율',
+//                 subtext: 'kcal 기준',
+//                 left: 'center'
+//             },
+//             tooltip: {
+//                 trigger: 'item',
+//                 formatter: '{a} <br/>{b} : {c} ({d}%)'
+//             },
+//             legend: {
+//                 left: 'center',
+//                 top: 'bottom',
+//                 data: [
+//                 'rose1',
+//                 'rose2',
+//                 'rose3',
+//                 'rose4',
+//                 'rose5'
+//                 ]
+//             },
+//             toolbox: {
+//                 show: true,
+//                 feature: {
+//                 mark: { show: true },
+//                 dataView: { show: true, readOnly: false },
+//                 restore: { show: true },
+//                 saveAsImage: { show: true }
+//                 }
+//             },
+//             series: [
+//                 {
+//                 name: 'Radius Mode',
+//                 type: 'pie',
+//                 radius: [20, 140],
+//                 center: ['25%', '50%'],
+//                 roseType: 'radius',
+//                 itemStyle: {
+//                     borderRadius: 5
+//                 },
+//                 label: {
+//                     show: false
+//                 },
+//                 emphasis: {
+//                     label: {
+//                     show: true
+//                     }
+//                 },
+//                 data: [
+//                     { value: 38.8, name: '탄수화물' },
+//                     { value: 24.3, name: '단백질' },
+//                     { value: 34.0, name: '지방' },
+//                     { value: 2.3, name: '식이섬유' },
+//                     { value: 0.6, name: '당' }
+//                 ]
+//                 },
+//                 {
+//                 name: 'Area Mode',
+//                 type: 'pie',
+//                 radius: [20, 140],
+//                 center: ['75%', '50%'],
+//                 roseType: 'area',
+//                 itemStyle: {
+//                     borderRadius: 5
+//                 },
+//                 data: [
+//                     { value: 30, name: 'rose 1' },
+//                     { value: 28, name: 'rose 2' },
+//                     { value: 26, name: 'rose 3' },
+//                     { value: 24, name: 'rose 4' },
+//                     { value: 22, name: 'rose 5' },
+//                     { value: 20, name: 'rose 6' },
+//                     { value: 18, name: 'rose 7' },
+//                     { value: 16, name: 'rose 8' }
+//                 ]
+//                 }
+//             ]
+//             };
+//             option && myChart.setOption(option);
