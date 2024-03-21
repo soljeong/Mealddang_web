d53ca28c7cfc5990d88c303bdcf9fc3193272640
byepingu <3149nelle@gmail.com>
2024-03-21 16:31:58 +0900

    footer에 팀원 영문명 추가

--- src/main/java/com/example/mealddang/controller/MdDietController.java
+++ src/main/java/com/example/mealddang/controller/MdDietController.java
@@ -63,8 +63,6 @@ public class MdDietController {
         // 오늘 날짜 (변하지 않는거)
         model.addAttribute("today", today);
         
-
-
         // 월요일 가져옴
         LocalDate monday = startDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
         model.addAttribute("monday", monday);
@@ -185,8 +183,4 @@ public class MdDietController {
         return "diet/resultPage";
     }
 
-    @GetMapping("/user/diet/nutrition")
-    public List<Object[]> getNutritionData(@RequestParam("username") String username, @RequestParam("dayofweek") int dayofweek) {
-        return mdNutResultRepository.sumNutDaily(username, dayofweek);
-    }
 }
